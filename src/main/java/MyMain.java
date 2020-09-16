25a1129cb8862b35f1e9605d2220b3423700abab
jmskeoch <jmskeoch@yahoo.com>
2020-09-16 17:25:54 +0000

    Add code to binaryToDecimal() method
    Implement user input

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int input = Integer.parseInt(binary);
+        int man = 1;
+        int result = 0;
+    
+        while(input > 0) {
+            result += (input % 10) * man;
+            input /= 10;
+            man *= 2;
+        }
+
+        return result;
     }
     
-    
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a binary number: ");
+        String input = scan.next();
+        System.out.println("The decimal equivalent to " + input + " is " + binaryToDecimal(input));
+
+        scan.close();
     }
 }
